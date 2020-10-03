3ee2adf1dc7ca6cd411b6fe566bd36653c39fb25
Oroko <aoroko09@gmail.com>
2020-10-03 15:18:08 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,12 @@
 def oxford_comma(array)
   if array.length == 1
-    array.join()
+     array.join()
   elsif array.length == 2
-    array.join(' and ')
-  else 
-    
-    [array[0..-2].join(', '), array.last].join(', and ')
-
-  
+    array.join(" and ")
+  else
+    names = [array[0..-2].join(", ")].join()
+    last_name = array[-1]
+    new_names = "#{names}, and #{last_name}"
     
   end
-  
 end
